1c1fe444bb4c106cdf3f49f198b8e0d0a3263361
Anatolii Moshko <anatoliimoshko@gmail.com>
2024-02-03 10:16:48 +0300

    added @NotNull, @NotBlank and @Positive

--- src/main/java/com/example/rest/rest/web/model/UpsertOrderRequest.java
+++ src/main/java/com/example/rest/rest/web/model/UpsertOrderRequest.java
@@ -1,5 +1,7 @@
 package com.example.rest.rest.web.model;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +12,16 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpsertOrderRequest {
+
+    @NotNull(message = "ID клиента должно быть указано!")
+    @Positive(message = "ID клиента должно быть больше 0!")
     private Long clientId;
+
+    @NotNull(message = "Имя продукта должно быть указано!")
     private String product;
+
+    @NotNull(message = "Цена должна быть указана!")
+    @Positive(message = "Цена должна быть больше 0!")
     private BigDecimal cost;
 
 }
